f5758748b4055a191b5c2d80360b36908c1019d0
madhava3279 <madhava.327889@gmail.com>
2024-10-31 14:15:33 +0000

    deploy_committed

--- deploy_snowpark_apps.py
+++ deploy_snowpark_apps.py
@@ -7,7 +7,7 @@ import yaml
 directory_path= sys.argv[1]
 
 os.chdir(f"{directory_path}")
-# Make sure all 6 SNOWFLAKE_ environment variables are set
+# Make sure all 6 SNOWFLAKE_ environment variables are setting
 # SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
 os.system(f"snow snowpark build")
 os.system(f"snow snowpark deploy --replace --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE")
